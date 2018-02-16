2c71f48ac1dcc1f97e30ff88b8891f172db8358a
Rupesh Kumar <rupesh.kumar@nagarro.com>
2018-02-16 13:25:15 +0530

    Updated Project

---
+++ XcodeServerBotApp/XcodeServerBotApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XcodeServerBotApp
+//
+//  Created by Rupesh Kumar on 16/02/18.
+//  Copyright © 2018 Rupesh Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
