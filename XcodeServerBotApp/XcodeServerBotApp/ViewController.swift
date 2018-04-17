f67a6a23e0b48a32eb1fcd527072c64426d40530
Rupesh Kumar <soft.rup17@gmail.com>
2018-04-17 12:49:04 +0530

    commit 5

--- XcodeServerBotApp/XcodeServerBotApp/ViewController.swift
+++ XcodeServerBotApp/XcodeServerBotApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Rupesh 3"
+        self.title = "Rupesh 5"
         // Do any additional setup after loading the view, typically from a nib.
     }
     
