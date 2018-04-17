e74ecff9a9a2bf028205bc42d67cfdae21b599dc
Rupesh Kumar <soft.rup17@gmail.com>
2018-04-17 12:48:29 +0530

    push 3

--- XcodeServerBotApp/XcodeServerBotApp/ViewController.swift
+++ XcodeServerBotApp/XcodeServerBotApp/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Rupesh 2"
+        self.title = "Rupesh 3"
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            self.title = "Rupesh 3"
+            // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
